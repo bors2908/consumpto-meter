3743ccc27074049e7ba5f92cf27d5c9ae84a4ecb
Boris_Ivanov <bors2908@gmail.com>
2022-04-15 20:22:52 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/consumpto/meter/ConsumptoMeterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.consumpto.meter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsumptoMeterApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConsumptoMeterApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/consumpto/meter/ConsumptoMeterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.consumpto.meter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumptoMeterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
