ee6d84108e0cf80441321eff75dd4187f67c6980
Boris_Ivanov <bors2908@gmail.com>
2022-04-19 18:59:40 +0300

    Move string processing to utils

--- src/main/kotlin/com/example/consumpto/meter/Utils.kt
+++ src/main/kotlin/com/example/consumpto/meter/Utils.kt
@@ -4,6 +4,7 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.TextStyle
 import java.util.Locale
 
 fun BigDecimal.currencyScale(): BigDecimal = this.setScale(2, RoundingMode.HALF_UP)
@@ -13,3 +14,11 @@ fun String.capitalize() = this.replaceFirstChar {
 }
 
 fun LocalDate.toYearMonth(): YearMonth = YearMonth.of(this.year, this.month)
+
+fun YearMonth.toYearMonthLocalizedString(): String {
+    val monthName = this.month
+        .getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault())
+        .capitalize()
+
+    return "$monthName ${this.year}"
+}
--- src/main/kotlin/com/example/consumpto/meter/dto/DtoMapper.kt
+++ src/main/kotlin/com/example/consumpto/meter/dto/DtoMapper.kt
@@ -1,12 +1,10 @@
 package com.example.consumpto.meter.dto
 
-import com.example.consumpto.meter.capitalize
 import com.example.consumpto.meter.domain.FuelRefill
 import com.example.consumpto.meter.domain.FuelStat
 import com.example.consumpto.meter.domain.FuelType
+import com.example.consumpto.meter.toYearMonthLocalizedString
 import java.time.YearMonth
-import java.time.format.TextStyle
-import java.util.Locale
 import org.springframework.stereotype.Component
 
 @Component
@@ -22,15 +20,15 @@ class DtoMapper {
     }
 
     fun <V> mapMonthKeys(map: Map<YearMonth, V>): Map<String, V> {
-        return map.mapKeys { yearMonthToString(it.key) }
+        return map.mapKeys { it.key.toYearMonthLocalizedString() }
     }
 
     fun mapRefills(map: Map<YearMonth, List<FuelRefill>>): Map<String, List<RefillDto>> {
-        return map.entries.associate { yearMonthToString(it.key) to it.value.map { refill -> refillToDto(refill) } }
+        return map.entries.associate { it.key.toYearMonthLocalizedString() to it.value.map { refill -> refillToDto(refill) } }
     }
 
     fun mapStats(map: Map<YearMonth, Map<FuelType, FuelStat>>): Map<String, List<StatDto>> {
-        return map.entries.associate { yearMonthToString(it.key) to statToDto(it.value) }
+        return map.entries.associate { it.key.toYearMonthLocalizedString() to statToDto(it.value) }
     }
 
     private fun refillToDto(refill: FuelRefill): RefillDto {
@@ -54,13 +52,5 @@ class DtoMapper {
                     stat.totalPrice
                 )
             }
-    }
-
-    private fun yearMonthToString(month: YearMonth): String {
-        val monthName = month.month
-            .getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault())
-            .capitalize()
-
-        return "$monthName ${month.year}"
     }
 }
